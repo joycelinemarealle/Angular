4125a4ab29567d30b5a4652eacb9c2de9698ec74
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-10-01 17:03:56 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'homes-app-1';
+  title = 'Welcome to HairByJoyceline';
 }
