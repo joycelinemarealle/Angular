dd7276de773dc11beb2d4c7ca3ddccfe80b64828
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-10-02 13:34:12 -0400

    image logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './home/home.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: "./app.component.html",
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Welcome to HairByJoyceline';
