664b562a08fdf87f7bba6e0fb3024b5cc1ebcc8d
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-10-04 10:58:52 -0400

    housing location links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from './home/home.component'
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterModule, HomeComponent],
   templateUrl: "./app.component.html",
   styleUrls: ['./app.component.css'],
 })
