ad14ae7ff3c36fdfdd0d4c139a384b9cdaeacb2b
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-10-04 17:25:19 -0400

    housing location links

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [],
+  templateUrl: "./app.details.html",
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+
+}
